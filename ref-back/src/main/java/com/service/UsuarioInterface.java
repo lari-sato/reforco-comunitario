7d998ae1323814a87d091ef15609f192331af0ad
Larissa Sato <159383822+lari-sato@users.noreply.github.com>
2025-10-29 08:24:53 -0300

    Adicionado imports faltando

--- ref-back/src/main/java/com/service/UsuarioInterface.java
+++ ref-back/src/main/java/com/service/UsuarioInterface.java
@@ -2,6 +2,10 @@ package com.example.service;
 
 
 import com.example.model.Usuario;
+import com.example.model.Insrutor;
+
+import java.util.List;
+
 
 public interface UsuarioInterface {
     void save(Usuario usuario);
