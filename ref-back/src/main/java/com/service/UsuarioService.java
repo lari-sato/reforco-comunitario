3852326d8fe14d7f26b65fa219b26c1dc25d3566
Larissa Sato <159383822+lari-sato@users.noreply.github.com>
2025-10-29 08:18:34 -0300

    Removido código inutilizado

--- ref-back/src/main/java/com/service/UsuarioService.java
+++ ref-back/src/main/java/com/service/UsuarioService.java
@@ -6,24 +6,12 @@ import com.example.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
-
 import java.util.List;
 
 @Service
 public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    public boolean registerUser(Usuario usuario) {
-        if (usuarioRepository.buscarPorEmail(usuario.getEmail()) != null) return false; // Se o e-mail já existe retorna falso
-        usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
-        usuarioRepository.save(usuario);
-        return true;
-    }
 
     public Usuario findByEmail(String email) {
         return usuarioRepository.buscarPorEmail(email);
