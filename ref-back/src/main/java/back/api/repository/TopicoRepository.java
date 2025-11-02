07a298340cbd8e447959280ef4e5f583d55d3bf7
Larissa Sato <larissa.sato@mackenzista.com.br>
2025-11-02 00:41:31 -0300

    Update TopicoRepository

---
+++ ref-back/src/main/java/back/api/repository/TopicoRepository.java
@@ -0,0 +1,9 @@
+package back.api.repository;
+
+import back.api.model.entity.Topico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+}
